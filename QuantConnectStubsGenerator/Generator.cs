679f0d006588f0275d17f057d700ffa9a76bf620
Jasper van Merle <jaspervmerle@gmail.com>
2020-10-30 02:55:41 +0100

    Properly format clr types

--- QuantConnectStubsGenerator/Generator.cs
+++ QuantConnectStubsGenerator/Generator.cs
@@ -252,17 +252,22 @@ import System.Reflection
 def getPreload() -> bool:
     ...
 
+
 def setPreload(preloadFlag: bool) -> None:
     ...
+
 
 def AddReference(name: str) -> System.Reflection.Assembly:
     ...
 
+
 def GetClrType(type: typing.Type[typing.Any]) -> System.Type:
     ...
 
+
 def FindAssembly(name: str) -> str:
     ...
+
 
 def ListAssemblies(verbose: bool) -> typing.List[System.Reflection.Assembly]:
     ...

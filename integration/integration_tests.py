07b747d867273291fc3da6854fe32b0cda385eb2
Martin-Molinero <martin@quantconnect.com>
2024-09-06 10:25:17 -0300

    Use python Enum (#25)
    - Minor adjustment so we use Python Enum class instead of CSharp system
      enum for stubs, it's better supported by mypy

--- integration/integration_tests.py
+++ integration/integration_tests.py
@@ -34,7 +34,7 @@ def main():
         file.write(f"""
 {{
     "include": [{", ".join([f'"{ns}/**"' for ns in os.listdir(stubs_dir)])}],
-    "exclude": ["System/Collections/Immutable/**", "System/__init__.pyi", "System/Runtime/InteropServices/__init__.pyi"],
+    "exclude": ["System/Collections/Immutable/**", "System/__init__.pyi", "System/Runtime/InteropServices/**"],
     "reportGeneralTypeIssues": "none",
     "reportInvalidTypeVarUse": "none",
     "reportWildcardImportFromLibrary": "none"

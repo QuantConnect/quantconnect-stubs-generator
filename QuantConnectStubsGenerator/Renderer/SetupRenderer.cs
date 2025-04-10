30809d3cbb11f8dd8045ce1b0d32a00dd44a4453
Josue Nina <josue.nina.ponce@gmail.com>
2025-04-10 09:34:23 -0500

    Add headers to the files

--- QuantConnectStubsGenerator/Renderer/SetupRenderer.cs
+++ QuantConnectStubsGenerator/Renderer/SetupRenderer.cs
@@ -1,3 +1,18 @@
+/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -88,7 +103,7 @@ setup(
             var files = tagsDirectory
                 .GetFiles()
                 .Select(file => file.Name)
-                .Select(name => int.TryParse(name, out var n) ? n : (int?) null)
+                .Select(name => int.TryParse(name, out var n) ? n : (int?)null)
                 .Where(tag => tag.HasValue)
                 .Select(tag => tag.Value)
                 .OrderBy(tag => tag)

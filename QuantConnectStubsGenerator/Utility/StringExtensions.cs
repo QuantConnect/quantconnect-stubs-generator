bc14b9b5354a532b56bf97d644ba1434d6819939
Martin-Molinero <martin@quantconnect.com>
2025-04-29 11:03:18 -0300

    Add missing python reserved name (#68)

--- QuantConnectStubsGenerator/Utility/StringExtensions.cs
+++ QuantConnectStubsGenerator/Utility/StringExtensions.cs
@@ -24,7 +24,8 @@ namespace QuantConnectStubsGenerator.Utility
         {
             "and", "as", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except",
             "False", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "None",
-            "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield"
+            "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield",
+            "await"
         };
 
         public static string Indent(this string str, int level = 1)
